78b1d61a88165e696a8b16602e39150dbcafc524
KingCosmic <KingCosmicDe@gmail.com>
2019-12-25 01:10:27 -0600

    Updates

---
+++ precache-manifest.0594d4e8fdf8cef71683bf800f834ac2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83357c8452546eb261db57a6b620fbc9",
+    "url": "/BardsBallad/index.html"
+  },
+  {
+    "revision": "e5cc081d173cfc90edfa",
+    "url": "/BardsBallad/static/css/main.36ab39b1.chunk.css"
+  },
+  {
+    "revision": "10fb34fa24e51057e1ba",
+    "url": "/BardsBallad/static/js/2.5a59d8e5.chunk.js"
+  },
+  {
+    "revision": "e5cc081d173cfc90edfa",
+    "url": "/BardsBallad/static/js/main.caca974a.chunk.js"
+  },
+  {
+    "revision": "a9cd164ff4af42cd3f12",
+    "url": "/BardsBallad/static/js/runtime~main.3bfed974.js"
+  },
+  {
+    "revision": "507906229d794aee1c5be34e1b0fd054",
+    "url": "/BardsBallad/static/media/Nunito-Bold.50790622.ttf"
+  },
+  {
+    "revision": "889c5cf0c4b919bfb66d09df46d314dd",
+    "url": "/BardsBallad/static/media/Nunito-Light.889c5cf0.ttf"
+  },
+  {
+    "revision": "11781884e3a96b39d8c6cd0e2bdaa40a",
+    "url": "/BardsBallad/static/media/Nunito-Regular.11781884.ttf"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/BardsBallad/static/media/OpenSans-Bold.50145685.ttf"
+  },
+  {
+    "revision": "8bac22ed4fd7c8a30536be18e2984f84",
+    "url": "/BardsBallad/static/media/OpenSans-ExtraBold.8bac22ed.ttf"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/BardsBallad/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/BardsBallad/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/BardsBallad/static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "f7213e1dda669c5cda41024d9df8ea14",
+    "url": "/BardsBallad/static/media/delete.f7213e1d.svg"
+  },
+  {
+    "revision": "c8faf3be6ff6c33c57098ff46788e5c5",
+    "url": "/BardsBallad/static/media/edit.c8faf3be.svg"
+  }
+]);
